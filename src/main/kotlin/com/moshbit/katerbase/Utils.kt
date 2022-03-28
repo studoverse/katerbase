78544542aa7747c7e01a57b7d83e719d1bbae09b
Zoltán Sasvári <sasvarizoli@me.com>
2022-03-28 14:21:20 +0200

    better index names with partial indexes (#10)
    * better index names with partial indexes
    
    * Update src/main/kotlin/com/moshbit/katerbase/Utils.kt
    
    Co-authored-by: Valentin Slawicek <valentin.slawi@gmail.com>
    
    Co-authored-by: Valentin Slawicek <valentin.slawi@gmail.com>

--- src/main/kotlin/com/moshbit/katerbase/Utils.kt
+++ src/main/kotlin/com/moshbit/katerbase/Utils.kt
@@ -5,6 +5,8 @@ import ch.qos.logback.classic.Logger
 import org.apache.commons.codec.binary.Hex
 import org.slf4j.impl.StaticLoggerBinder
 import java.security.MessageDigest
+import java.text.SimpleDateFormat
+import java.util.*
 
 internal fun setLogLevel(name: String, level: Level) {
   (StaticLoggerBinder.getSingleton().loggerFactory.getLogger(name) as Logger).level = level
@@ -18,4 +20,10 @@ internal fun ByteArray.sha256(): String {
 
 internal fun String.sha256(): String {
   return this.toByteArray().sha256()
+}
+
+internal fun Date.toIsoString(): String {
+  return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    .apply { timeZone = TimeZone.getTimeZone("UTC") }
+    .format(this)
 }
