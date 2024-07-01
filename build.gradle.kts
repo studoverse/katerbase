6be0e275e47c082c2cbde2ca3175c64ab0cd6125
Fabian Schedler <fabian.schedler@studo.com>
2024-07-01 14:51:25 +0200

    Update jackson & logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ dependencies {
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-  implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+  implementation("com.fasterxml.jackson.core:jackson-core:2.17.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
 
-  implementation("ch.qos.logback:logback-classic:1.2.11")
+  implementation("ch.qos.logback:logback-classic:1.5.6")
 
   implementation("commons-codec:commons-codec:1.15")
 
